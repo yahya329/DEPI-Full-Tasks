5693dbb2e7ecf4f9262d75fe5387e53c1d163a90
yahya <yahya214465@gmail.com>
2026-03-14 09:32:02 +0200

    EF Labs update

---
+++ Task_5.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Lab
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+
+    public class myApp : DbContext
+    {
+
+        public DbSet<Book> Books { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=LibraryDb;Trusted_Connection=True;");
+        }
+    }
+}
